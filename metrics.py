570b94d728f249d6b1c595f16079237ac07db923
aymenkhs <aymenkhouas@gmail.com>
2022-04-27 18:03:57 +0100

    [feat] add the rougeN metric that takes as parameter the referance and the predicted sentence

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
-from nltk.util import ngrams
+from nltk.util import ngrams, everygrams
 
 # f-function score
 f_score_function = lambda precision, recall : 2 * ((precision * recall) / (precision + recall))
@@ -19,3 +19,17 @@ def rouge_n(referance, sentence, n=1):
 
 
   return recall, precision, f_score
+
+
+def rouge_l(reference, sentence):
+  referance_ngrams = everygrams(reference.split())
+  sentence_ngrams = everygrams(sentence.split())
+
+  referance_ngrams = [item for item in referance_ngrams]
+  sentence_ngrams = [item for item in sentence_ngrams]
+
+  matching_ngrams = [item for item in sentence_ngrams if item in referance_ngrams]
+
+  longuest_common_substring = len(matching_ngrams[-1])
+
+  return longuest_common_substring
