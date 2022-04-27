283c7bcca0182c72b868c5a7a149f95a81aa6537
aymenkhs <aymenkhouas@gmail.com>
2022-04-27 18:52:46 +0100

    [add] evaluation init

---
+++ evaluations.py
@@ -0,0 +1,19 @@
+from metrics import rouge_n, rouge_l
+
+def compute_rouge_n(evalaution_data):
+    pass
+
+def compute_rouge_l(evalaution_data):
+    pass
+
+def compute_rouge_we(evalaution_data):
+    pass
+
+def compute_bert_score(evalaution_data):
+    pass
+
+def compute_meteor(evalaution_data):
+    pass
+
+def compute_chrf(evalaution_data):
+    pass
