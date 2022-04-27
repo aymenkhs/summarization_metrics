c314b8719fb4c99b235175fcc110832595c3b863
aymenkhs <aymenkhouas@gmail.com>
2022-04-27 19:09:25 +0100

    [refactor] fix typo in compute metrics parameter

--- evaluations.py
+++ evaluations.py
@@ -1,19 +1,19 @@
 from metrics import rouge_n, rouge_l
 
-def compute_rouge_n(evalaution_data):
+def compute_rouge_n(evaluation_data):
     pass
 
-def compute_rouge_l(evalaution_data):
+def compute_rouge_l(evaluation_data):
     pass
 
-def compute_rouge_we(evalaution_data):
+def compute_rouge_we(evaluation_data):
     pass
 
-def compute_bert_score(evalaution_data):
+def compute_bert_score(evaluation_data):
     pass
 
-def compute_meteor(evalaution_data):
+def compute_meteor(evaluation_data):
     pass
 
-def compute_chrf(evalaution_data):
+def compute_chrf(evaluation_data):
     pass
