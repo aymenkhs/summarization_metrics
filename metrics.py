3bb8408a1b3929cdfc1f40934457dfa35b630dc8
aymenkhs <aymenkhouas@gmail.com>
2022-04-27 18:05:18 +0100

    [code-style] fix indentations

--- metrics.py
+++ metrics.py
@@ -5,31 +5,31 @@ f_score_function = lambda precision, recall : 2 * ((precision * recall) / (preci
 
 
 def rouge_n(referance, sentence, n=1):
-  referance_ngrams = ngrams(reference.split(), n)
-  sentence_ngrams = ngrams(sentence.split(), n)
+    referance_ngrams = ngrams(reference.split(), n)
+    sentence_ngrams = ngrams(sentence.split(), n)
 
-  referance_ngrams = [item for item in referance_ngrams]
-  sentence_ngrams = [item for item in sentence_ngrams]
+    referance_ngrams = [item for item in referance_ngrams]
+    sentence_ngrams = [item for item in sentence_ngrams]
 
-  matching_ngrams = [item for item in sentence_ngrams if item in referance_ngrams]
+    matching_ngrams = [item for item in sentence_ngrams if item in referance_ngrams]
 
-  recall = len(matching_ngrams) / len(sentence_ngrams)
-  precision = len(matching_ngrams) / len(referance_ngrams)
-  f_score = f_score_function(precision, recall)
+    recall = len(matching_ngrams) / len(sentence_ngrams)
+    precision = len(matching_ngrams) / len(referance_ngrams)
+    f_score = f_score_function(precision, recall)
 
 
-  return recall, precision, f_score
+    return recall, precision, f_score
 
 
 def rouge_l(reference, sentence):
-  referance_ngrams = everygrams(reference.split())
-  sentence_ngrams = everygrams(sentence.split())
+    referance_ngrams = everygrams(reference.split())
+    sentence_ngrams = everygrams(sentence.split())
 
-  referance_ngrams = [item for item in referance_ngrams]
-  sentence_ngrams = [item for item in sentence_ngrams]
+    referance_ngrams = [item for item in referance_ngrams]
+    sentence_ngrams = [item for item in sentence_ngrams]
 
-  matching_ngrams = [item for item in sentence_ngrams if item in referance_ngrams]
+    matching_ngrams = [item for item in sentence_ngrams if item in referance_ngrams]
 
-  longuest_common_substring = len(matching_ngrams[-1])
+    longuest_common_substring = len(matching_ngrams[-1])
 
-  return longuest_common_substring
+    return longuest_common_substring
