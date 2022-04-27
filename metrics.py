e2ad76af0cebe6cb92c98c3d12cf12578a5b9b8c
aymenkhs <aymenkhouas@gmail.com>
2022-04-27 18:47:05 +0100

    [fix] fix typo in referance's variable name

--- metrics.py
+++ metrics.py
@@ -4,7 +4,7 @@ from nltk.util import ngrams, everygrams
 f_score_function = lambda precision, recall : 2 * ((precision * recall) / (precision + recall))
 
 def rouge_n(referance, sentence, n=1):
-    referance_ngrams = ngrams(reference.split(), n)
+    referance_ngrams = ngrams(referance.split(), n)
     sentence_ngrams = ngrams(sentence.split(), n)
 
     referance_ngrams = [item for item in referance_ngrams]
