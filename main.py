9c964856eea265c926c67cd70088a082345f1163
aymenkhs <aymenkhouas@gmail.com>
2022-04-28 21:44:00 +0100

    [refactor] remove default value to path argument in read_eval_data function

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 import evaluations
 
-# TODO: remove default evaluation data
-def read_eval_data(path = 'evaluation_data/meaning_cloud_samcorpus_test_results.json'):
+def read_eval_data(path):
     data = pd.read_json(path, orient='index')
     # TODO: standerdize the columns with 'prediction' and 'referance'
     data = data[['result_summary', 'true_summary']]
     return data
 
 def main():
-    eval_data = read_eval_data()
+    eval_data = read_eval_data('evaluation_data/meaning_cloud_samcorpus_test_results.json')
     eval_data.columns = ['prediction', 'referance']
     evaluations.compute_rouge_n(eval_data)
     import pdb; pdb.set_trace()
